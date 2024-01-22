d13edd9d2be126fe69a756ad78f3d946d61858ae
Taxus <75935258+Taxus-max@users.noreply.github.com>
2024-01-22 21:08:02 +0100

    Create exchange functionality basics

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -4,6 +4,15 @@ import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {MatButtonModule} from "@angular/material/button";
 import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import worldCurrencies from "../../assets/currency_symbol.json"
+
+interface currency {
+  Flag: string,
+  CountryName: string,
+  Currency: string,
+  Code: string,
+  Symbol: string
+}
 
 @Component({
   selector: 'app-exchange',
@@ -19,6 +28,25 @@ import {MatGridList, MatGridTile} from "@angular/material/grid-list";
   templateUrl: './exchange.component.html',
   styleUrl: './exchange.component.css'
 })
-export class ExchangeComponent {
 
+export class ExchangeComponent {
+  currencies: currency[]= worldCurrencies;
+  currencyFrom: string = "HUF";
+  currencyTo: string = "";
+
+  convertButtonClick(moneyStartingAmount: string){
+    if (this.currencyFrom === this.currencyTo) {
+      alert("Please select two different currencies!")
+      return;
+    }
+    if (Number(moneyStartingAmount) === 0){
+      alert("Please define amount to exchange!")
+      return;
+    }
+    if (this.currencyTo === ""){
+      alert("Please select the the currency to convert!")
+      return;
+    }
+
+  };
 }
