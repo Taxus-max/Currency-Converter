d7e6864eebe48c137e1e5cd8c671896dc01b4aff
Taxus <75935258+Taxus-max@users.noreply.github.com>
2024-01-22 14:39:49 +0100

    Create a basic design for the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatGridList,
+    MatGridTile,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
