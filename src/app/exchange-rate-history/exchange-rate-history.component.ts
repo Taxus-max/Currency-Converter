bd5432b69ce2291bbd936e1bdde5677f25bd9740
Taxus <75935258+Taxus-max@users.noreply.github.com>
2024-01-23 17:29:34 +0100

    Add appearance for exchange rate history

--- src/app/exchange-rate-history/exchange-rate-history.component.ts
+++ src/app/exchange-rate-history/exchange-rate-history.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, numberAttribute} from '@angular/core';
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+
+interface rateCurrencies{
+  USD: number,
+  EUR: number,
+  YEN: number,
+  AUD: number,
+  GBP: number,
+  CHF: number,
+  CAD: number,
+  HKD: number,
+  NZD: number,
+  CNH: number
+
+}
 
 @Component({
   selector: 'app-exchange-rate-history',
   standalone: true,
-  imports: [],
+  imports: [
+    MatGridTile,
+    MatGridList
+  ],
   templateUrl: './exchange-rate-history.component.html',
   styleUrl: './exchange-rate-history.component.css'
 })
 export class ExchangeRateHistoryComponent {
-
+  selectedCurrency: string = "";
+  exchangeRates: rateCurrencies = {
+    USD: 0,
+    EUR: 0,
+    YEN: 0,
+    AUD: 0,
+    GBP: 0,
+    CHF: 0,
+    CAD: 0,
+    HKD: 0,
+    NZD: 0,
+    CNH: 0
+  }
 }
