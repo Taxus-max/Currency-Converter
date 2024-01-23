ff71db190cc50ecc97ca110eb1b5619cb0faf53b
Taxus <75935258+Taxus-max@users.noreply.github.com>
2024-01-23 14:26:17 +0100

    Create a service to handle exchanges

---
+++ src/app/currency-data.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {Subject} from "rxjs";
+
+interface exchangeDetails{
+  from: string,
+  to: string,
+  amount: number,
+  amountMiddle?: number,
+  finalAmount: number
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CurrencyDataService {
+  latestExchangeDetails: exchangeDetails = {
+    from: '',
+    to: '',
+    amount: 0,
+    finalAmount: 0
+  };
+  latestExchangeDetailsChange: Subject<exchangeDetails> = new Subject<exchangeDetails>();
+
+  constructor() {
+    this.latestExchangeDetailsChange.subscribe((value => {
+      this.latestExchangeDetails = value
+    }))
+  }
+
+  convertCurrency(amount: number, from: string, to: string){
+    if(to === "EUR"){
+      //Exchange from - to
+      //Return only the final amount
+    }else{
+      //Exchange from - eur - to
+      //Return amount after both steps
+    }
+
+    this.latestExchangeDetailsChange.next(this.latestExchangeDetails = {
+      from: "HUF",
+      to: "EUR",
+      amount: 1000,
+      finalAmount: 10400
+    });
+  }
+
+}
