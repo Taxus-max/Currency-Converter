bd60b80dc42febd00cf9309ddb60f32c67b4c746
Taxus <75935258+Taxus-max@users.noreply.github.com>
2024-01-24 14:00:41 +0100

    Create a component to display current rates

--- src/app/exchange-rate-current/exchange-rate-current.component.ts
+++ src/app/exchange-rate-current/exchange-rate-current.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+
+interface currentExchangeRates{
+  usdToEur: number,
+  eurToUsd: number,
+  usdTogbp: number,
+  gbpTousd: number,
+  eurTogbp: number,
+  gbpToEur: number,
+}
 
 @Component({
   selector: 'app-exchange-rate-current',
   standalone: true,
-  imports: [],
+  imports: [
+    MatGridTile,
+    MatGridList
+  ],
   templateUrl: './exchange-rate-current.component.html',
   styleUrl: './exchange-rate-current.component.css'
 })
 export class ExchangeRateCurrentComponent {
-
+  currentExchangeRates: currentExchangeRates = {
+    usdToEur: 0,
+    eurToUsd: 0,
+    usdTogbp: 0,
+    gbpTousd: 0,
+    eurTogbp: 0,
+    gbpToEur: 0,
+  }
 }
